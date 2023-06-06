84af4e1e8e637e559919cf5a9771cc99adeab0b3
kiwi-ui <yusnianggara@gmail.com>
2023-06-06 14:34:59 +0700

    feat: add create method

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,104 @@
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 function App() {
-  const onSubmit = () => {
-    console.log('halo')
-  }
+  const [testimonials, setTestimonials] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    asalInstansi: '',
+    testimoni: '',
+    photo:''
+  });
+
+  useEffect(() => {
+    fetchTestimonials();
+  }, []);
+
+  const fetchTestimonials = async () => {
+    try {
+      const response = await axios.get('https://647ed0d7c246f166da8f68b6.mockapi.io/testi');
+      setTestimonials(response.data);
+      console.log(response)
+    } catch (error) {
+      console.error('Error fetching testimonials:', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('https://647ed0d7c246f166da8f68b6.mockapi.io/testi', formData);
+      const newTestimonial = response.data;
+
+      setTestimonials((prevTestimonials) => [...prevTestimonials, newTestimonial]);
+      setFormData({ name: '', email: '', asalInstansi: '', testimoni: '', photo:'' });
+    } catch (error) {
+      console.error('Error creating testimonial:', error);
+    }
+  };
 
   return (
-    <>
-      <main className="d-flex justify-content-center flex-column vw-100 vh-100 align-items-center pt-3 pt-lg-5">
-        <form className="h-75 d-flex justify-content-center flex-column" onSubmit={onSubmit}>
-          <div className="mb-4">
-            <label htmlFor="name" className="form-label fsemibold">Nama</label>
-            <input type="text" className="form-control" id="name" autoComplete="name" required />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="form-label fsemibold">Email</label>
-            <input type="email" className="form-control" id="email" autoComplete="email" required />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="asalInstansi" className="form-label fsemibold">Asal Instansi</label>
-            <input type="text" className="form-control" id="asalInstansi" autoComplete="asal-instansi" required />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="testimoni" className="form-label fsemibold">Testimoni</label>
-            <textarea className="form-control" id="testimoni" autoComplete="testimoni" required></textarea>
-          </div>
+    <main className="d-flex justify-content-center flex-column vw-100 vh-100 align-items-center pt-3 pt-lg-5">
+      <form className="h-75 w-50 d-flex justify-content-center flex-column" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="name" className="form-label fsemibold">Nama</label>
+          <input type="text" className="form-control" id="name" name="name" value={formData.name} onChange={handleInputChange} autoComplete="name" required />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="form-label fsemibold">Email</label>
+          <input type="email" className="form-control" id="email" name="email" value={formData.email} onChange={handleInputChange} autoComplete="email" required />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="asalInstansi" className="form-label fsemibold">Asal Instansi</label>
+          <input type="text" className="form-control" id="asalInstansi" name="asalInstansi" value={formData.asalInstansi} onChange={handleInputChange} autoComplete="asal-instansi" required />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="testimoni" className="form-label fsemibold">Testimoni</label>
+          <textarea className="form-control" id="testimoni" name="testimoni" value={formData.testimoni} onChange={handleInputChange} autoComplete="testimoni" required></textarea>
+        </div>
+        <div className="mb-4">
+          <label htmlFor="photo" className="form-label fsemibold">Photo</label>
+          <input className="form-control" id="photo" name="photo" value={formData.photo} onChange={handleInputChange} autoComplete="testimoni" required />
+        </div>
+        
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
 
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-      </main>
-    </>
+      {testimonials.length > 0 && (
+        <div className="mt-5 overflow-scroll">
+          <h2>Testimonials</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Nama</th>
+                <th scope="col">Email</th>
+                <th scope="col">Asal Instansi</th>
+                <th scope="col">Testimoni</th>
+                <th scope="col">Foto</th>
+              </tr>
+            </thead>
+            <tbody>
+              {testimonials.map((testimonial, index) => (
+                <tr key={index}>
+                  <td>{testimonial.name}</td>
+                  <td>{testimonial.email}</td>
+                  <td>{testimonial.asalInstansi}</td>
+                  <td>{testimonial.testimoni}</td>
+                  <td>{testimonial.photo}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </main>
   );
 }
 
